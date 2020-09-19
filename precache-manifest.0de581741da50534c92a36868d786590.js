dcacdcc0480efd6138ecdeb7bf52b1b290fae03a
nicolo cervo <nicolo.cervo@gmail.com>
2020-09-19 17:58:04 +0200

    Updates

---
+++ precache-manifest.0de581741da50534c92a36868d786590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2688773bea029606acc10f5a211f93cf",
+    "url": "/testreactapp/index.html"
+  },
+  {
+    "revision": "e22198eacede82d23590",
+    "url": "/testreactapp/static/css/main.5e899f23.chunk.css"
+  },
+  {
+    "revision": "7b3287c65a36681af3eb",
+    "url": "/testreactapp/static/js/2.fa9dc631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testreactapp/static/js/2.fa9dc631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22198eacede82d23590",
+    "url": "/testreactapp/static/js/main.abca6ee1.chunk.js"
+  },
+  {
+    "revision": "8e391896d507c09c799b",
+    "url": "/testreactapp/static/js/runtime-main.4e04770f.js"
+  },
+  {
+    "revision": "3f88980cf3e7b829fa5376de88eb689f",
+    "url": "/testreactapp/static/media/dog.3f88980c.jpg"
+  }
+]);
